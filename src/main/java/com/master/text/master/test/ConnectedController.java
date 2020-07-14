e78a2a35a946d478a44edfea2dc10ebd52133718
Maneesh <maneeshraj2000@gmail.com>
2020-07-13 21:37:20 -0500

    Added Swagger API doc

--- src/main/java/com/master/text/master/test/ConnectedController.java
+++ src/main/java/com/master/text/master/test/ConnectedController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 /**
  * Rest Controller to expose the connected service 
  * @author maneesh
  *
  */
+@ApiOperation(value = "Connected API to return yes if the given 2 cities are connected", response = String.class)
 @RestController
 public class ConnectedController{
 	
